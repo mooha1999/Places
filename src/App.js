6ded8eed44c6bbdddbd53abe8f0aabd2a3213fa6
mooha1999 <50667005+mooha1999@users.noreply.github.com>
2021-10-22 14:15:39 +0200

    Adding the 'Auth' component

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import {BrowserRouter as
   Redirect,
   Switch
   } from 'react-router-dom';
+import Auth from 'user/pages/Auth';
 import MainNavigation from './shared/components/Navigation/MainNavigation';
 import Users from './user/pages/Users';
 const App = () => 
@@ -26,6 +27,9 @@ const App = () =>
       </Route>
       <Route path="/places/:placeId">
         <UpdatePlace/>
+      </Route>
+      <Route path="/auth">
+        <Auth/>
       </Route>
       <Redirect to="/"/>
       </Switch>
---
+++ src/user/pages/Auth.js
@@ -0,0 +1,59 @@
+import React from "react";
+import Button from "shared/components/FormElements/Button";
+import Input from "shared/components/FormElements/Input";
+import Card from "shared/components/UIElements/Card";
+import { useForm } from "shared/hooks/form-hook";
+import { VALIDATOR_EMAIL, VALIDATOR_MINLENGTH } from "shared/util/validators";
+
+import "./Auth.css";
+
+const Auth = () => {
+  const [formState, inputHandler] = useForm({
+    email: {
+      value: '',
+      isValid: false
+    },
+    password: {
+      value: '',
+      isValid: false
+    }
+  }, false);
+  const authSubmitHandler = e => {
+    e.preventDefault();
+    console.log(formState.inputs);
+  }
+  return (
+    <Card className="authentication">
+      <h2>Login</h2>
+      <hr />
+      <form onSubmit = {authSubmitHandler}>
+        <Input
+          element="input"
+          id="email"
+          type="email"
+          label="E-mail"
+          validators={[VALIDATOR_EMAIL()]}
+          errorText="Please enter a valid email address"
+          onInput={inputHandler}
+        />
+        <Input
+          element="input"
+          id="password"
+          type="password"
+          label="Password"
+          validators={[VALIDATOR_MINLENGTH(8)]}
+          errorText="Password should be at least 8 characters"
+          onInput={inputHandler}
+        />
+        <Button
+          type="submit"
+          disabled={!formState.isValid}
+        >
+          Login
+        </Button>
+      </form>
+    </Card>
+  );
+};
+
+export default Auth;
